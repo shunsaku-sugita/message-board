6cdea248cfb7ee642b7dec9935469f956d14d761
hot.spring <saku.dot@gmail.com>
2017-08-23 12:35:55 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,11 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  
   def index
     @messages = Message.all
   end
   
   def show
-    @message = Message.find(params[:id])
   end
   
   def new
@@ -24,12 +25,9 @@ class MessagesController < ApplicationController
   end
   
   def edit
-    @message = Message.find(params[:id])
   end
   
   def update
-    @message = Message.find(params[:id])
-    
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
       redirect_to @message
@@ -40,18 +38,20 @@ class MessagesController < ApplicationController
   end
   
   def destroy
-    @message = Message.find(params[:id])
     @message.destroy
     
     flash[:success] = 'Message は正常に削除されました'
     redirect_to messages_url
   end
+
+  private
   
-end
+  def set _message
+    @message = Message.find(params[:id])
+  end
 
-private
-
-# Strong Parameter
-def message_params
-  params.require(:message).permit(:content)
+  # Strong Parameter
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
